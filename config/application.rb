78d744806b105ad70e04cd1573ca96b723c9ad02
mrwilsondj <mrwilsondj@gmail.com>
2014-05-15 16:17:01 -0400

    rails app on heroku

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module InstaHeroku
   class Application < Rails::Application
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -22,4 +23,4 @@ module InstaHeroku
   end
 end
 
-config.assets.initialize_on_precompile = false
+
